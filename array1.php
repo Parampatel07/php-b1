8daf14a35aa99498fe1f3845175e86216db055d2
Parampatel07 <90458806+Parampatel07@users.noreply.github.com>
2022-09-07 18:25:14 +0530

    array

---
+++ array1.php
@@ -0,0 +1,19 @@
+<?php
+$car=array();
+var_dump($car);
+print_r($car);
+$car=array('bmw','benz','audi');
+var_dump($car);
+print_r($car);
+
+$car[3]='JDm';
+var_dump($car);
+
+echo $car[2];
+$car[5]=10;
+var_dump($car);
+$car[4]=25;
+var_dump($car);
+print_r($car);
+
+?>
---
+++ array2.php
@@ -0,0 +1,33 @@
+<!-- write a programe to sort an array -->
+<?php
+$number=array();
+
+$num=rand(10,99);
+echo $num;
+$count=0;
+while($count<=7)
+{
+    $number[$count]=rand(10,99);
+    $count++;
+}
+var_dump($number);
+$flash=0;
+while($flash<=7)
+{
+    $count=$flash+1;
+    while($count<=7)
+    {
+        if($number[$flash]<$number[$count])
+        {
+            $temp=$number[$flash];
+            $number[$flash]=$number[$count];
+            $number[$count]=$temp;
+        }
+        $count++;
+    }
+    $flash++;
+}
+
+
+var_dump($number);
+?>
